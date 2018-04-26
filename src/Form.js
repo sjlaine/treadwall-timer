c032506e88f3d5039b1e888ca79cc9deb20a537e
Sarah Laine <sjlaine624@gmail.com>
2018-04-26 18:17:20 -0400

    added click handler to buttons

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 export default class Form extends Component {
+  constructor() {
+    super();
+    this.state = {};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    console.log(event.target);
+    const activeEls = [...document.getElementsByClassName('active')];
+    console.log(activeEls);
+    activeEls.forEach(el => el.classList.remove('active'));
+    event.target.classList.add('active');
+  }
   render() {
     return (
       <div>
@@ -8,10 +22,32 @@ export default class Form extends Component {
           Create a Custom Timer
         </h1>
         <h3>Route Color:</h3>
-        <button className="red-btn">Red</button>
-        <button className="yellow-btn">Yellow</button>
-        <button className="green-btn">Green</button>
-        <button className="blue-btn">Blue</button>
+        <div>
+          <button
+            className="red-btn"
+            onClick={this.handleClick}
+          >
+            Red
+          </button>
+          <button
+            className="yellow-btn"
+            onClick={this.handleClick}
+          >
+            Yellow
+          </button>
+          <button
+            className="green-btn"
+            onClick={this.handleClick}
+          >
+            Green
+          </button>
+          <button
+            className="blue-btn"
+            onClick={this.handleClick}
+          >
+            Blue
+          </button>
+        </div>
       </div>
     )
   }
