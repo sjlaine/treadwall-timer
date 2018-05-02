8f9ecff16e6bf89b1f3ef76f512b5ee655ff3d94
Sarah Laine <sjlaine624@gmail.com>
2018-05-01 22:40:41 -0400

    fixed unused vars

--- src/Interval.js
+++ src/Interval.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import NewTimer from './NewTimer';
 
 export default class Interval extends Component {
   constructor(props) {
@@ -11,23 +10,11 @@ export default class Interval extends Component {
     }
 
     this.handleChange = this.handleChange.bind(this);
-    // this.parseTime = this.parseTime.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
     this.setState({[event.target.name]: event.target.value});
   }
-
-  // parseTime() {
-  //   const timeArr = this.state.value.split(':');
-  //   let hours = timeArr[timeArr.length - 3] || '00';
-  //   let minutes = timeArr[timeArr.length - 2] || '00';
-  //   let seconds = timeArr[timeArr.length - 1] || '00';
-  //   const parsed = {hours: +hours, minutes: +minutes, seconds: +seconds};
-
-  //   return parsed;
-  // }
 
   render() {
     const repeats = this.state.repeats;
